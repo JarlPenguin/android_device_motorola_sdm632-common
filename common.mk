f7be3a56b17de68974b2a1de5c20ec313d68024a
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    sdm632-common: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde

--- common.mk
+++ common.mk
@@ -404,11 +404,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl:64 \
-    android.hardware.thermal@1.0-service
-
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
