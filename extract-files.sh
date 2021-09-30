46e2694f43bd9142ef8b31c794eb88a896df96b4
Jarl-Penguin <jarlpenguin@outlook.com>
2021-09-30 14:18:24 +0300

    fixup! sdm632-common: Add a check before adding dependencies
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- extract-files.sh
+++ extract-files.sh
@@ -68,7 +68,7 @@ function blob_fixup() {
         # qsap shim
         vendor/lib64/libmdmcutback.so)
             for  LIBQSAP_SHIM in $(grep -L "libqsap_shim.so" "${2}"); do
-                $"{PATCHELF}" --add-needed "libqsap_shim.so" "$LIBQSAP_SHIM"
+                "${PATCHELF}" --add-needed "libqsap_shim.so" "$LIBQSAP_SHIM"
             done
             ;;
     esac
